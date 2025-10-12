c3decad8e121a22ac8b82784cfd089a74419d92d
Michelle Han <michellehan2007agt@gmail.com>
2025-10-12 18:34:48 -0400

    separated pages

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-$(document).ready(function() {
+// // $(document).ready(function() {
 
-  var $body = $('html, body');
-  var $tags = $('#goto-mission, #goto-facts, #goto-programs, #goto-events, #goto-exec, #goto-sponsors, #goto-resources, #goto-connect');
+// //   var $body = $('html, body');
+// //   var $tags = $('#index.html, #goto-facts, #goto-programs, #goto-events, #goto-exec, #goto-sponsors, #goto-resources, #goto-connect');
 
-  $tags.click(function(e) {
-    var elementName = e.target.id.substr(5);
-    $body.animate({
-      scrollTop: $('#'+elementName).offset().top - 60
-    }, 800);
-  });
-});
+// //   $tags.click(function(e) {
+// //     var elementName = e.target.id.substr(5);
+// //     $body.animate({
+// //       scrollTop: $('#'+elementName).offset().top - 60
+// //     }, 800);
+// //   });
+// // });
 
+// $(document).ready(function() {
+//   $('#navbar .item').click(function(e) {
+//     e.preventDefault(); // Prevent default anchor behavior
+//     var page = $(this).attr('id');
+//     window.location.href = page; // Navigate to the page
+//   });
+// });
