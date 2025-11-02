96edbb1efd9003088580622d4a16208a4626bcbe
michellehhan <michellehan2007agt@gmail.com>
2025-11-02 16:34:27 -0500

    exec layout + new members

--- js/data.js
+++ js/data.js
@@ -1,213 +1,239 @@
 const SPONSORS_DATA = {
-    sponsors: [
-      {
-        name: "Citadel",
-        url: "https://www.citadel.com/",
-        image: "public/images/sponsors/citadel.png",
-        tier: "silver",
-      },
-      {
-        name: "PDT Partners",
-        url: "https://pdtpartners.com/",
-        image: "public/images/sponsors/pdt.png",
-        tier: "silver",
-      },
-      {
-        name: "Jump Trading",
-        url: "https://www.jumptrading.com/",
-        image: "public/images/sponsors/jump.png",
-        tier: "silver",
-      },
-      {
-        name: "Capital One",
-        url: "https://www.capitalone.com/",
-        image: "public/images/sponsors/c1.png",
-        tier: "silver",
-      },
-      {
-        name: "MIT-Harvard COOP",
-        url: "https://www.thecoop.com/?srsltid=AfmBOooASK0iEwZUQT4O-eXkLZL9wY43r5sLc0XupWZRPIGAENnGoio7",
-        image: "public/images/sponsors/mitcoop.svg",
-        tier: "past",
-      },
-      {
-        name: "D.E. Shaw",
-        url: "https://www.deshaw.com/",
-        image: "public/images/sponsors/deshaw.png",
-        tier: "bronze",
-      },
-      {
-        name: "Roblox",
-        url: "https://www.roblox.com/",
-        image: "public/images/sponsors/roblox.png",
-        tier: "past",
-      },
-      {
-        name: "Persona",
-        url: "https://withpersona.com/",
-        image: "public/images/sponsors/persona.png",
-        tier: "past",
-      },
-      {
-        name: "Susquehanna",
-        url: "https://sig.com/",
-        image: "public/images/sponsors/sig.png",
-        tier: "bronze",
-      },
-      {
-        name: "Jane Street",
-        url: "https://www.janestreet.com/",
-        image: "public/images/sponsors/janestreet.png",
-        tier: "bronze",
-      },
-      {
-        name: "Symbotic",
-        url: "https://www.symbotic.com/",
-        image: "public/images/sponsors/symbotic.png",
-        tier: "silver",
-      },
-      {
-        name: "Data Dog",
-        url: "https://www.datadoghq.com/",
-        image: "public/images/sponsors/datadog.png",
-        tier: "bronze",
-      },
+  sponsors: [
+    {
+      name: "Citadel",
+      url: "https://www.citadel.com/",
+      image: "public/images/sponsors/citadel.png",
+      tier: "silver",
+    },
+    {
+      name: "PDT Partners",
+      url: "https://pdtpartners.com/",
+      image: "public/images/sponsors/pdt.png",
+      tier: "silver",
+    },
+    {
+      name: "Jump Trading",
+      url: "https://www.jumptrading.com/",
+      image: "public/images/sponsors/jump.png",
+      tier: "silver",
+    },
+    {
+      name: "Capital One",
+      url: "https://www.capitalone.com/",
+      image: "public/images/sponsors/c1.png",
+      tier: "silver",
+    },
+    {
+      name: "MIT-Harvard COOP",
+      url: "https://www.thecoop.com/?srsltid=AfmBOooASK0iEwZUQT4O-eXkLZL9wY43r5sLc0XupWZRPIGAENnGoio7",
+      image: "public/images/sponsors/mitcoop.svg",
+      tier: "past",
+    },
+    {
+      name: "D.E. Shaw",
+      url: "https://www.deshaw.com/",
+      image: "public/images/sponsors/deshaw.png",
+      tier: "bronze",
+    },
+    {
+      name: "Roblox",
+      url: "https://www.roblox.com/",
+      image: "public/images/sponsors/roblox.png",
+      tier: "past",
+    },
+    {
+      name: "Persona",
+      url: "https://withpersona.com/",
+      image: "public/images/sponsors/persona.png",
+      tier: "past",
+    },
+    {
+      name: "Susquehanna",
+      url: "https://sig.com/",
+      image: "public/images/sponsors/sig.png",
+      tier: "bronze",
+    },
+    {
+      name: "Jane Street",
+      url: "https://www.janestreet.com/",
+      image: "public/images/sponsors/janestreet.png",
+      tier: "bronze",
+    },
+    {
+      name: "Symbotic",
+      url: "https://www.symbotic.com/",
+      image: "public/images/sponsors/symbotic.png",
+      tier: "silver",
+    },
+    {
+      name: "Data Dog",
+      url: "https://www.datadoghq.com/",
+      image: "public/images/sponsors/datadog.png",
+      tier: "bronze",
+    },
 
 
-    ],
-  };
+  ],
+};
 
-  const EVENTS_DATA = {
-    events: [
-      {
-        title: "Cookie Decorating Study Break",
-        date: "10/30/2025",
-        image: "public/images/events/2025-2026/cookie_decorating.png",
-      },
-      {
-        title: "Playa Bowls Study Break",
-        date: "10/2/2025",
-        image: "public/images/events/2025-2026/playa_bowls.png",
-      },
-      {
-        title: "Hot Chocolate Study Break",
-        date: "2/28/2025",
-        image: "public/images/events/2024-2025/hot_chocolate.png",
-      },
-      {
-        title: "Matcha Study Break",
-        date: "11/8/2024",
-        image: "public/images/events/2024-2025/matcha_study_break.png",
-      },
-      {
-        title: "Google Speaker Event",
-        date: "10/29/2024",
-        image: "public/images/events/2024-2025/google_speaker_event.png",
-      },
-      {
-        title: "Citadel Women's Breakfast",
-        date: "9/18/2024",
-        image: "public/images/sponsors/citadel.png",
-      },
-      {
-        title: "Mentorship Spring Kickoff",
-        date: "3/10/2024",
-        image: "public/images/events/2023-2024/mentorship_spring.png",
-      },
-      {
-        title: "Charceuterie Study Break",
-        date: "3/21/2024",
-        image: "public/images/events/2023-2024/charcuterie.png",
-      },
-      {
-        title: "Tufts ECE x MIT WiEECS Mixer",
-        date: "3/1/2024",
-        image: "public/images/events/2023-2024/tufts_mixer.png",
-      },
-      {
-        title: "Movie Night",
-        date: "1/27/2024",
-        image: "public/images/events/2023-2024/MovieNight.png",
-      },
-    ],
-  };
+const EVENTS_DATA = {
+  events: [
+    {
+      title: "Cookie Decorating Study Break",
+      date: "10/30/2025",
+      image: "public/images/events/2025-2026/cookie_decorating.png",
+    },
+    {
+      title: "Playa Bowls Study Break",
+      date: "10/2/2025",
+      image: "public/images/events/2025-2026/playa_bowls.png",
+    },
+    {
+      title: "Hot Chocolate Study Break",
+      date: "2/28/2025",
+      image: "public/images/events/2024-2025/hot_chocolate.png",
+    },
+    {
+      title: "Matcha Study Break",
+      date: "11/8/2024",
+      image: "public/images/events/2024-2025/matcha_study_break.png",
+    },
+    {
+      title: "Google Speaker Event",
+      date: "10/29/2024",
+      image: "public/images/events/2024-2025/google_speaker_event.png",
+    },
+    {
+      title: "Citadel Women's Breakfast",
+      date: "9/18/2024",
+      image: "public/images/sponsors/citadel.png",
+    },
+    {
+      title: "Mentorship Spring Kickoff",
+      date: "3/10/2024",
+      image: "public/images/events/2023-2024/mentorship_spring.png",
+    },
+    {
+      title: "Charceuterie Study Break",
+      date: "3/21/2024",
+      image: "public/images/events/2023-2024/charcuterie.png",
+    },
+    {
+      title: "Tufts ECE x MIT WiEECS Mixer",
+      date: "3/1/2024",
+      image: "public/images/events/2023-2024/tufts_mixer.png",
+    },
+    {
+      title: "Movie Night",
+      date: "1/27/2024",
+      image: "public/images/events/2023-2024/MovieNight.png",
+    },
+  ],
+};
 
-  const PEOPLE_DATA = {
-    executives: [
-      {
-        name: "Sabrina Do",
-        role: "Co-President",
-        image: "public/images/exec/2024-2025/Sabrina_Do.jpg",
-      },
-      {
-        name: "Shiqi Cheng",
-        role: "Co-President",
-        image: "public/images/exec/2024-2025/Shiqi_Cheng.jpg",
-      },
-      {
-        name: "Janelle Cai",
-        role: "Treasurer",
-        image: "public/images/exec/2023-2024/Janelle_Cai.jpeg",
-      },
-      {
-        name: "Arianna Scott",
-        role: "Community Events Team",
-        image: "public/images/exec/2024-2025/Arianna_Scott.jpg",
-      },
-      {
-        name: "Christy Li",
-        role: "Community Events Team",
-        image: "public/images/exec/2024-2025/Christy_Li.jpg",
-      },
-      {
-        name: "Kyra Henriques",
-        role: "Community Events Team",
-        image: "public/images/exec/2024-2025/Kyra_Henriques.jpg",
-      },
-      {
-        name: "Rachel Jiang",
-        role: "Community Events Team",
-        image: "public/images/exec/2024-2025/Rachel_Jiang.jpg",
-      },
-      {
-        name: "Angelika Wang",
-        role: "Mentorship Team",
-        image: "public/images/exec/2024-2025/Angelika_Wang.jpg",
-      },
-      {
-        name: "Catherine Li",
-        role: "Mentorship Team",
-        image: "public/images/exec/2024-2025/Catherine_Li.jpg",
-      },
-      {
-        name: "Iris Xu",
-        role: "Mentorship Team",
-        image: "public/images/exec/2024-2025/Iris_Xu.jpg",
-      },
-      {
-        name: "Valerie Kwek",
-        role: "Mentorship Team",
-        image: "public/images/exec/2024-2025/Valerie_Kwek.jpg",
-      },
-      {
-        name: "Isabella Wu",
-        role: "Professional Development Team",
-        image: "public/images/exec/2024-2025/Isabella_Wu.jpg",
-      },
-      {
-        name: "Jeewoo Kang",
-        role: "Professional Development Team",
-        image: "public/images/exec/2024-2025/Jeewoo_Kang.jpg",
-      },
-      {
-        name: "Nikita Agarwal",
-        role: "Professional Development Team",
-        image: "public/images/exec/2024-2025/Nikita_Agarwal.jpg",
-      },
-      {
-        name: "Dianne Cao",
-        role: "Publicity",
-        image: "public/images/exec/2024-2025/Dianne_Cao.jpg",
-      },
-    ],
-  };
+const PEOPLE_DATA = {
+  executives: [
+    
+  {
+    "name": "Sabrina Do",
+    "role": "Co-President",
+    "image": "public/images/exec/2024-2025/Sabrina_Do.jpg"
+  },
+  {
+    "name": "Shiqi Cheng",
+    "role": "Co-President",
+    "image": "public/images/exec/2024-2025/Shiqi_Cheng.jpg"
+  },
+  {
+    "name": "Janelle Cai",
+    "role": "Treasurer",
+    "image": "public/images/exec/2023-2024/Janelle_Cai.jpeg"
+  },
+  {
+    "name": "Arianna Scott",
+    "role": "Community Events Team",
+    "image": "public/images/exec/2024-2025/Arianna_Scott.jpg"
+  },
+  {
+    "name": "Christy Li",
+    "role": "Community Events Team",
+    "image": "public/images/exec/2024-2025/Christy_Li.jpg"
+  },
+  {
+    "name": "Kyra Henriques",
+    "role": "Community Events Team",
+    "image": "public/images/exec/2024-2025/Kyra_Henriques.jpg"
+  },
+  {
+    "name": "Claire Chen",
+    "role": "Community Events Team",
+    "image": "public/images/exec/2024-2025/Claire_Chen.jpg"
+  },
+  {
+    "name": "Rachel Yang",
+    "role": "Community Events Team",
+    "image": "public/images/exec/2024-2025/Rachel_Yang.jpg"
+  },
+  {
+    "name": "Isabella Wu",
+    "role": "Development Team",
+    "image": "public/images/exec/2024-2025/Isabella_Wu.jpg"
+  },
+  {
+    "name": "Michelle Han",
+    "role": "Development Team",
+    "image": "public/images/exec/2024-2025/Michelle_Han.jpg"
+  },
+  {
+    "name": "Angela Yang",
+    "role": "Development Team",
+    "image": "public/images/exec/2024-2025/Angela_Yang.jpg"
+  },
+  {
+    "name": "Catherine Li",
+    "role": "Mentorship Team",
+    "image": "public/images/exec/2024-2025/Catherine_Li.jpg"
+  },
+  {
+    "name": "Iris Xu",
+    "role": "Mentorship Team",
+    "image": "public/images/exec/2024-2025/Iris_Xu.jpg"
+  },
+  {
+    "name": "Maureen Zhang",
+    "role": "Mentorship Team",
+    "image": "public/images/exec/2024-2025/Maureen_Zhang.jpg"
+  },
+  {
+    "name": "Chinmayi Goyal",
+    "role": "Mentorship Team",
+    "image": "public/images/exec/2024-2025/Chinmayi_Goyal.jpg"
+  },
+  {
+    "name": "Rachel Jiang",
+    "role": "Professional Development Team",
+    "image": "public/images/exec/2024-2025/Rachel_Jiang.jpg"
+  },
+  {
+    "name": "Jeewoo Kang",
+    "role": "Professional Development Team",
+    "image": "public/images/exec/2024-2025/Jeewoo_Kang.jpg"
+  },
+  {
+    "name": "Nikita Agrawal",
+    "role": "Professional Development Team",
+    "image": "public/images/exec/2024-2025/Nikita_Agarwal.jpg"
+  },
+  {
+    "name": "Valeria Ramirez",
+    "role": "Professional Development Team",
+    "image": "public/images/exec/2024-2025/Valeria_Ramirez.jpg"
+  },
+  {
+    "name": "Jada Ogueh",
+    "role": "Professional Development Team",
+    "image": "public/images/exec/2024-2025/Jada_Ogueh.jpg"
+  }
+  ],
+};
